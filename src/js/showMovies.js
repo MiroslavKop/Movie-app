2a998d3764b9bf5326b87e26d96006a5f0f45a29
Serhii Kononov <admin@ip-192-168-85-186.eu-west-1.compute.internal>
2022-10-12 21:50:41 +0300

    refactoring

--- src/js/showMovies.js
+++ src/js/showMovies.js
@@ -1,24 +1,31 @@
-import {getVoteColor} from "./getColor";
-import {API_KEY, BASE_URL, API_URL, IMG_URL, SEARCH_URL, getMovies} from "./api";
-import {modalWindow, openModal} from "./modalWindow";
+import { getVoteColor } from "./getColor";
+import { IMG_URL } from "./api";
+import { modalWindow, openModal } from "./modalWindow";
+
+const movies = document.querySelector(".movies");
 
 export function showMovies(data) {
-    let movies = document.querySelector(".movies");
-    movies.innerHTML = "";
-    data.forEach((movie) => {
-        let movieEl = document.createElement("div");
+  movies.innerHTML = "";
+  data.forEach((movie) => {
+    let movieEl = document.createElement("div");
 
-        movieEl.classList.add("movie");
-        movieEl.innerHTML = `
-                <div class="movie__wrapper"><img src="${IMG_URL + movie.poster_path}" alt=${movie.title} class="movie__poster"></div>
+    movieEl.classList.add("movie");
+    // Bad practise - need to rewrite. I'll add the idea
+    movieEl.innerHTML = `
+                <div class="movie__wrapper"><img src="${
+                  IMG_URL + movie.poster_path
+                }" alt=${movie.title} class="movie__poster"></div>
                 <div class="movie__info">
                     <h2 class="movie__info-title">${movie.title}</h2>
-                    <div class="movie__info-raiting ${getVoteColor(movie.vote_average)}">${movie.vote_average}</div>
+                    <div class="movie__info-raiting ${getVoteColor(
+                      movie.vote_average
+                    )}">${movie.vote_average}</div>
                 </div>
     `;
-        movieEl.addEventListener("click", () => openModal(movie.id));
-        movies.append(movieEl);
-    });
+
+    // Event delegation!!!
+    movieEl.addEventListener("click", () => openModal(movie.id));
+
+    movies.append(movieEl);
+  });
 }
-
-
